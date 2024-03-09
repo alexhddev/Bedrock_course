5fbba24b2c05974087caa013eb7ae981fc82c618
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-03-09 17:01:01 +0200

    py text models

---
+++ py/src/text/generate.py
@@ -0,0 +1,38 @@
+import boto3
+import json
+import pprint
+
+client = boto3.client(service_name='bedrock-runtime', region_name="us-west-2")
+
+titan_model_id = 'amazon.titan-text-express-v1'
+
+titan_config = json.dumps({
+            "inputText": "Tell me a story about a dragon",
+            "textGenerationConfig": {
+                "maxTokenCount": 4096,
+                "stopSequences": [],
+                "temperature": 0,
+                "topP": 1
+            }
+        })
+
+llama_model_id = "meta.llama2-13b-chat-v1"
+llama_config = json.dumps({
+    "prompt": "Tell me a story about a dragon",
+    "max_gen_len": 512,
+    "temperature": 0,
+    "top_p": 0.9,
+})
+
+response = client.invoke_model(
+    body=llama_config,
+    modelId=llama_model_id,
+    accept="application/json",
+    contentType="application/json"
+)
+
+response_body = json.loads(response.get('body').read())
+
+pp = pprint.PrettyPrinter(depth=4)
+# pp.pprint(response_body.get('results')) # titan config
+pp.pprint(response_body["generation"]) # llama config
