27b75689db73d8bf96cb31aae1df48007c745c88
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-03-20 11:36:42 +0200

    Basic RAG app - PY

---
+++ py/src/langchain/basic_rag.py
@@ -0,0 +1,62 @@
+from langchain_community.llms import Bedrock
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.vectorstores import FAISS
+import boto3
+
+my_data = [
+    "The weather is nice today.",
+    "Last night's game ended in a tie.",
+    "Don likes to eat pizza.",
+    "Don likes to eat pasta.",
+]
+
+question = "What does Don like to eat?"
+
+AWS_REGION = "us-west-2"
+
+bedrock = boto3.client(service_name="bedrock-runtime", region_name=AWS_REGION)
+
+model = Bedrock(model_id="amazon.titan-text-express-v1", client=bedrock)
+
+bedrock_embeddings = BedrockEmbeddings(
+    model_id="amazon.titan-embed-text-v1", client=bedrock
+)
+
+# create vector store
+vector_store = FAISS.from_texts(my_data, bedrock_embeddings)
+
+# create retriever
+retriever = vector_store.as_retriever(
+    search_kwargs={"k": 2}  # maybe we can add a score threshold here?
+)
+
+results = retriever.get_relevant_documents(question)
+
+results_string = []
+for result in results:
+    results_string.append(result.page_content)
+
+# build template:
+template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Answer the users question based on the following context: {context}",
+        ),
+        ("user", "{input}"),
+    ]
+)
+
+chain = template.pipe(model)
+
+response = chain.invoke({"input": question, "context": results_string})
+print(response)
+
+
+
+
+
+
+
+
