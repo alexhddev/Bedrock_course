e3dd89a58329e00dd2d14752aeee0a11aaf5df94
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-03-20 10:54:42 +0200

    First chain - TS

---
+++ ts/src/langchain/FirstChain.ts
@@ -0,0 +1,32 @@
+import { Bedrock } from "@langchain/community/llms/bedrock";
+import { ChatPromptTemplate } from '@langchain/core/prompts'
+
+const AWS_REGION = 'us-west-2'
+
+const model = new Bedrock({
+    model: 'amazon.titan-text-express-v1',
+    region: AWS_REGION
+});
+
+async function invokeModel() {
+    const response = await model.invoke("What is the highest mountain in the world?");
+
+    console.log(response)
+}
+
+async function firstChain() {
+    const prompt = ChatPromptTemplate.fromMessages([
+        ['system', 'Write a short description for the product provided by the user'],
+        ['human', '{product_name}']
+    ])
+
+    const chain = prompt.pipe(model);
+
+    const response = await chain.invoke({
+        product_name: 'bicycle'
+    })
+    console.log(response)
+}
+
+firstChain()
+
