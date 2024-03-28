b3da8adb591d2d29c2e5f6b83d7d57a1d8d342cf
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-03-28 09:43:55 +0200

    IAC: Image api - CDK Python

--- imageApi/py/py/py_stack.py
+++ imageApi/py/py/py_stack.py
@@ -1,7 +1,10 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda,
+    aws_apigateway,
+    aws_s3,
+    aws_iam
 )
 from constructs import Construct
 
@@ -10,10 +13,32 @@ class PyStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        images_bucket = aws_s3.Bucket(self, 'PY-ImagesPyBucket')
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PyQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        images_lambda = aws_lambda.Function(
+            self,
+            "Py-ImageLambda",
+            runtime=aws_lambda.Runtime.PYTHON_3_11,
+            code=aws_lambda.Code.from_asset("services"),
+            handler="image.handler",
+            timeout=Duration.seconds(30),
+            environment={
+                "BUCKET_NAME": images_bucket.bucket_name
+            }
+        )
+
+        images_bucket.grant_read_write(images_lambda)
+
+        images_lambda.add_to_role_policy(aws_iam.PolicyStatement(
+            effect=aws_iam.Effect.ALLOW,
+            resources=["*"],
+            actions=["bedrock:InvokeModel"]
+        ))
+
+        api = aws_apigateway.RestApi(self, "Py-ImageApi")
+
+        image_resource = api.root.add_resource("image")
+        image_integration = aws_apigateway.LambdaIntegration(images_lambda)
+        image_resource.add_method("POST", image_integration)
+
+
--- imageApi/py/services/image.py
+++ imageApi/py/services/image.py
@@ -2,9 +2,10 @@ import boto3
 import json
 import base64
 from time import time
+import os
 
 AWS_REGION_BEDROCK = "us-west-2"
-S3_BUCKET = "images-bucket-111"
+S3_BUCKET = os.environ.get("BUCKET_NAME")
 
 client = boto3.client(service_name="bedrock-runtime", region_name=AWS_REGION_BEDROCK)
 s3_client = boto3.client('s3')
