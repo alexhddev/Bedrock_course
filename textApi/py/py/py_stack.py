05191253b5cde4d05587f60ed70ae1f6f53efa7f
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-04-03 16:15:05 +0300

    iac summary py

--- textApi/py/py/py_stack.py
+++ textApi/py/py/py_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_apigateway,
+    aws_lambda,
+    aws_iam
 )
 from constructs import Construct
 
@@ -10,10 +12,23 @@ class PyStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        summary_lambda = aws_lambda.Function(
+            self,
+            "Py-SummaryLambda",
+            runtime=aws_lambda.Runtime.PYTHON_3_11,
+            code=aws_lambda.Code.from_asset("services"),
+            handler="summary.handler",
+            timeout=Duration.seconds(30)
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PyQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        summary_lambda.add_to_role_policy(aws_iam.PolicyStatement(
+            effect=aws_iam.Effect.ALLOW,
+            resources=["*"],
+            actions=["bedrock:InvokeModel"]            
+        ))
+
+        api = aws_apigateway.RestApi(self, "PY-SummaryApi")
+
+        text_resource = api.root.add_resource("text")
+        summary_integration = aws_apigateway.LambdaIntegration(summary_lambda)
+        text_resource.add_method("POST", summary_integration)
