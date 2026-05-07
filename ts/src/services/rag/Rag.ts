8266854f3fea1b674e1b0cf58f130141fee49e4d
alexhddev <21276888+alexhddev@users.noreply.github.com>
2026-05-07 13:39:44 +0300

    Agents

--- ts/src/services/rag/Rag.ts
+++ ts/src/services/rag/Rag.ts
@@ -1,7 +1,7 @@
 import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda";
 import { BedrockAgentRuntimeClient, RetrieveAndGenerateCommand } from '@aws-sdk/client-bedrock-agent-runtime'
 
-const AWS_REGION_BEDROCK = "us-west-2";
+const AWS_REGION_BEDROCK = "us-east-1";
 
 const client = new BedrockAgentRuntimeClient({ region: AWS_REGION_BEDROCK });
 
@@ -19,7 +19,7 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
                     type: 'KNOWLEDGE_BASE',
                     knowledgeBaseConfiguration: {
                         knowledgeBaseId: 'PSX4JIJMQN',
-                        modelArn: 'arn:aws:bedrock:us-west-2::foundation-model/anthropic.claude-v2'
+                        modelArn: 'arn:aws:bedrock:us-east-1::inference-profile/us.anthropic.claude-3-haiku-20240307-v1:0'
                     }
                 }
             })
